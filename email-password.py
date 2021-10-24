c8b080d7f67aace8d33233d5c0de75df1b2f75ef
minced1 <jacagu477@gmail.com>
2021-10-24 12:27:26 +0200

    added special characters to passwords and updated GUI

--- email-password.py
+++ email-password.py
@@ -2,13 +2,17 @@ import names
 import random
 import string
 import tkinter
+from tkinter import *
 from tkinter import messagebox
 
 domain = ["gmail.com", "gmx.net", "web.de", "yahoo.com", "hotmail.com", "aol.com", 
     "hotmai.co.uk", "hotmail.fr", "msn.com", "yahoo.fr", "wanadoo.fr", "orange.fr", "comcast.net", 
     "yahoo.co.uk", "yahoo.com.br", "yahoo.co.in", "live.com", "rediffmail.com", "free.fr"]
 
+
 top = tkinter.Tk()
+top.title("Email-Password-Generator")
+
 def mailadress():
     
     name = names.get_full_name().replace(" ", ".").lower()
@@ -28,11 +32,37 @@ def rngPasswd():
 
     return messagebox.showinfo("RNG Password", "Password: " + passwd)
 
-B1 = tkinter.Button(top, text = "Email with a real name", command = mailadress)
-B2 = tkinter.Button(top, text = "random character Email", command = secondmail)
-B3 = tkinter.Button(top, text = "random generated Password", command = rngPasswd)
-B1.pack()
-B2.pack()
-B3.pack()
+def rngPasswdSpecial():
+
+    passwdsp = ''.join(random.choice(string.ascii_letters+string.digits+string.punctuation) for i in range(random.randint(10,16)))
+
+    return messagebox.showinfo("RNG Password", "Password: " + passwdsp)
+
+def genEmail():
+    var0b = var0.get()
+    if var0b == 0:
+        mailadress()
+    elif var0b == 1:
+        secondmail()
+    else:
+        print("email")
+
+def genPasswd():
+    var1b = var1.get()
+    if var1b == 0:
+        rngPasswd()
+    elif var1b == 1:
+        rngPasswdSpecial()
+    else:
+        print("passwd")
+
+var0, var1 = IntVar(), IntVar()
+R1 = tkinter.Radiobutton(top, text="Real name", variable=var0, value=0).grid(row = 1, column = 1, sticky="w")
+R2 = tkinter.Radiobutton(top, text="Random characters", variable=var0, value=1).grid(row = 2, column = 1, sticky="w")
+R3 = tkinter.Radiobutton(top, text="Without special characters", variable=var1, value=0).grid(row = 1, column = 2, sticky="w")
+R4 = tkinter.Radiobutton(top, text="With special characters", variable=var1, value=1).grid(row = 2, column = 2, sticky="w")
+B1 = tkinter.Button(top, text = "Generate Email", width = 20, command = genEmail).grid(row = 3, column = 1)
+B2 = tkinter.Button(top, text = "Generate Password", width = 20, command = genPasswd).grid(row = 3, column = 2)
+
 
 top.mainloop()
